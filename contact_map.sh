c0a6ae53898632c1f469b70085acb292cc9d66bc
Jay Ghurye <jay@Jays-MacBook-Pro.local>
2020-03-06 10:01:46 -0800

    comments added

--- contact_map.sh
+++ contact_map.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 bam=$1
-outputfile=$2
-asm=$3
+ref=$2
 tmpdir=/tmp
-prefix=$4
+prefix=$3
 #prefix=`basename  ${bam} | sed 's/.bam//g'`
 SCRIPT_PATH=`realpath $0`
 SCRIPT_PATH=`dirname $SCRIPT_PATH`
 
-samtools faidx $asm
-cut -f 1,2 $asm.fai > /tmp/chr_size.txt
+#create index of reference and chromosome size file
+samtools faidx $ref
+cut -f 1,2 $ref.fai > /tmp/chr_size.txt
 chrlength="/tmp/chr_size.txt"
+
+#convert the bam file into juicer compatible text file.
+#Make sure it's sorted appropriately in the format juicer requires
 python ${SCRIPT_PATH}/convert_alignments_to_tsv.py ${bam} ${chrlength} \
     | awk '{if ($2 > $6) {print $1"\t"$6"\t"$7"\t"$8"\t"$5"\t"$2"\t"$3"\t"$4} else {print}}' \
     | sort -k2,2d -k6,6d -T ${tmpdir} --parallel=40 \
     > ${tmpdir}/${prefix}_juicer_alignments.txt
 
+#Convert the sorted text file into .hic file
 java -jar -Xmx30g \
      ${SCRIPT_PATH}/juicertools.jar pre \
-    ${tmpdir}/${prefix}_juicer_alignments.txt ${outputfile} ${chrlength}
+    ${tmpdir}/${prefix}_juicer_alignments.txt ${prefix}.hic ${chrlength}
 
+#convert this generated hic file into cool file
 hic2cool convert -r 1000  ${outputfile} ${prefix}.cool
 
-cooler zoomify --balance -p 40 ${prefix}.cool
+#generate multi-resolution cooler file from 1kb resolution cooler file
+cooler zoomify --balance -p 4 ${prefix}.cool
