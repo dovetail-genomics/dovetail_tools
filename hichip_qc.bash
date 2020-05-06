67e858bd639025ff80f23df6d113ab5e42d4fa1b
Ubuntu <ubuntu@ip-172-31-53-84.us-west-2.compute.internal>
2020-05-06 16:04:01 +0000

    file changed based on name

--- hichip_qc.bash
+++ hichip_qc.bash
@@ -18,6 +18,9 @@ bedtools sort -g ${genome} -i ${peaks} > ${prefix}_reordered_peaks.bed
 
 bed=${prefix}"_reordered_peaks.bed"
 
+bed_chr20=${prefix}"_chr20_reordered_peaks.bed"
+grep -w 'chr20' ${bed} > ${bed_chr20}
+
 #compute how many reads intersect with peaks
 bedtools intersect -a ${bam} -b ${bed} -bed | sort -k4 > ${prefix}_peak_intersect.bed &
 bedtools window -w 500 -abam ${bam} -b ${peaks} -bed | sort -k4 > ${prefix}_peaks_intersect_500.bed &
@@ -29,16 +32,23 @@ wait
 
 #generate peak enrichment plot
 
-bamCoverage --bam ${bam} --outFileName  ${prefix}_coverage.bigwig --outFileFormat bigwig
+bamCoverage --bam ${bam} --outFileName  ${prefix}_coverage.bigwig --outFileFormat bigwig 
+
 computeMatrix reference-point --scoreFileName ${prefix}_coverage.bigwig --referencePoint \
  	center --beforeRegionStartLength 10000 --afterRegionStartLength 10000 \
- 	--outFileName ${prefix}_coverage.matrix.gz
+ 	--outFileName ${prefix}_coverage.matrix.gz  --regionsFileName ${bed_chr20} &
+
+plotFingerprint -b ${bam} --region chr20 --plotFile ${prefix}_chip_fingerprint_plot.png --outRawCounts ${prefix}_counts.tab &
+
+wait 
 
 plotProfile -m ${prefix}_coverage.matrix.gz --perGroup  -out ${prefix}_peak_enrichment.png
 
+python ${SRCDIR}/plot_chip_fingerprint.py -table ${prefix}_counts.tab/counts.tab -output ${prefix}_chip_fingerprint_plot.png 
+
 
 #print final stats
-${SRCDIR}/count.py -b1  ${prefix}_peak_intersect.bed -b2 ${prefix}_peaks_intersect_500.bed \
+python ${SRCDIR}/count.py -b1  ${prefix}_peak_intersect.bed -b2 ${prefix}_peaks_intersect_500.bed \
 	-b3 ${prefix}_peaks_intersect_1000.bed -b4 ${prefix}_peaks_intersect_2000.bed \
 	-b5 ${prefix}_peaks_intersect_5000.bed   -bam ${bam} -peaks ${peaks}
 
