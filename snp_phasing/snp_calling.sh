84a7e3706ae841e27d500af046ff4b079d23f0fb
Ubuntu <ubuntu@ip-172-31-53-84.us-west-2.compute.internal>
2020-05-14 14:26:05 +0000

    filename changed

--- snp_phasing/snp_calling.sh
+++ snp_phasing/snp_calling.sh
@@ -22,7 +22,7 @@ cut -f 1,2 ${ref}.fai > ${prefix}.contig_size.txt
 
 #Call SNPs with HaplotypeCaller
 
-gatk HaplotypeCaller -A RMSMappingQuality -R ${ref} -I ${bam} -O ${prefix}_raw_variants.vcf -ERC GVCF --native-pair-hmm-threads 1 --max-alternate-alleles 3 --QUIET 
+gatk HaplotypeCaller -A RMSMappingQuality -R ${ref} -I ${bam} -O ${prefix}_variants.vcf -ERC GVCF --native-pair-hmm-threads 1 --max-alternate-alleles 3 --QUIET 
 
 bgzip -c  ${prefix}_variants.vcf > ${prefix}_variants.vcf.gz 
 tabix ${prefix}_variants.vcf.gz
