ca1f32732cdcf1d6036dba0541c790b2c3f8300a
Ubuntu <ubuntu@ip-172-31-53-84.us-west-2.compute.internal>
2020-06-05 13:51:36 +0000

    pairtools works on both contact and alignment

--- contact_map.sh
+++ contact_map.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 
-bam=$1
+pairs=$1
 ref=$2
-tmpdir=/tmp
 prefix=$3
+cores=$4
 #prefix=`basename  ${bam} | sed 's/.bam//g'`
 SCRIPT_PATH=`realpath $0`
 SCRIPT_PATH=`dirname $SCRIPT_PATH`
 
 #create index of reference and chromosome size file
 samtools faidx $ref
-cut -f 1,2 $ref.fai > /tmp/chr_size.txt
-chrlength="/tmp/chr_size.txt"
+cut -f 1,2 $ref.fai > ${prefix}.genome
+chrlength="${prefix}.genome"
 
-#convert the bam file into juicer compatible text file.
-#Make sure it's sorted appropriately in the format juicer requires
-python ${SCRIPT_PATH}/convert_alignments_to_tsv.py ${bam} ${chrlength} \
-    | awk '{if ($2 > $6) {print $1"\t"$6"\t"$7"\t"$8"\t"$5"\t"$2"\t"$3"\t"$4} else {print}}' \
-    | sort -k2,2d -k6,6d -T ${tmpdir} --parallel=40 \
-    > ${tmpdir}/${prefix}_juicer_alignments.txt
+#convert the pairs  file into juicer compatible text file.
+zcat ${pairs} \
+	| grep -v "#" \
+	| grep "UU" \
+	| awk '{print "1\t"$2"\t"$3"\t1\t0\t"$4"\t"$5"\t0"}' \
+	| awk '{if ($2 > $6) {print $1"\t"$6"\t"$7"\t"$8"\t"$5"\t"$2"\t"$3"\t"$4} else {print}}' \
+	| sort -k2,2d -k6,6d  --parallel=${cores} > ${prefix}_juicer_alignments.txt
 
 #Convert the sorted text file into .hic file
 java -jar -Xmx30g \
      ${SCRIPT_PATH}/juicertools.jar pre \
-    ${tmpdir}/${prefix}_juicer_alignments.txt ${prefix}.hic ${chrlength}
+    ${prefix}_juicer_alignments.txt ${prefix}.hic ${chrlength}
 
-#convert this generated hic file into cool file
-hic2cool convert -r 1000  ${outputfile} ${prefix}.cool
+
+#now convert pairs to .hic
+pairix ${pairs}
+
+cooler cload pairix \
+	 -p ${cores} \
+	 ${prefix}.genome:1000 \
+	 ${pairs} \
+	 ${prefix}.cool
 
 #generate multi-resolution cooler file from 1kb resolution cooler file
-cooler zoomify --balance -p 4 ${prefix}.cool
+cooler zoomify --balance -p ${cores}  ${prefix}.cool
--- omni-c_qc.bash
+++ omni-c_qc.bash
@@ -19,37 +19,39 @@ bwa mem -5SP -T0 -t${cores} \
     $ref \
     $fq1 \
     $fq2 \
-| pairtools parse --output-stats ${outprefix}.PT.stats.txt  --chroms-path ${genome}\
+| pairtools parse --chroms-path ${genome}\
 | pairtools sort  --nproc ${cores} \
-| pairtools dedup --mark-dups \
-| pairtools split --output-pairs \
-  ${outprefix}.PT.pairs.gz  --output-sam ${outprefix}-PT.bam 
+| pairtools dedup --nproc-in ${cores} --nproc-out ${cores} --mark-dups  \
+ 	--output-stats ${outprefix}-PT.stats.txt \
+| pairtools split --nproc-in ${cores} --nproc-out ${cores} \
+	--output-pairs ${outprefix}.PT.pairs.gz  \
+	--output-sam - \
+| samtools view -bS - \
+| samtools sort -@${cores} - -o ${outprefix}-PT.bam
 
-samtools sort -@${cores} ${outprefix}-PT.bam -o ${outprefix}-PT.sorted.bam
-samtools index ${outprefix}-PT.sorted.bam;
+
+samtools index ${outprefix}-PT.bam;
 
 
-preseq lc_extrap -B -P -e 2.1e9 -s 1e8 -seg_len 1000000000 -o $outprefix.preseq ${outprefix}-PT.sorted.bam
+preseq lc_extrap -B -P -e 2.1e9 -s 1e8 -seg_len 1000000000 -o $outprefix.preseq ${outprefix}-PT.bam
 
 ps300m=`cat ${outprefix}.preseq | grep -P "^300000000.0" | awk '{print $2}'`
 
 qualthresh=40
 mate_filter_cmd="perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' $qualthresh"
 
-r1=`samtools view -c -q $qualthresh -f 0x40 -F 2304 ${outprefix}-PT.sorted.bam`
-r2=`samtools view -c -q $qualthresh -f 0x80 -F 2304 ${outprefix}-PT.sorted.bam`
+r1=`samtools view -c -q $qualthresh -f 0x40 -F 2304 ${outprefix}-PT.bam`
+r2=`samtools view -c -q $qualthresh -f 0x80 -F 2304 ${outprefix}-PT.bam`
 
-echo "samtools view -q $qualthresh -f 0x40 -F 2316 ${outprefix}-PT.sorted.bam |  $mate_filter_cmd | wc -l"
-mapped_pairs=`samtools view -q $qualthresh -f 0x40 -F 2316 ${outprefix}-PT.sorted.bam | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
-echo ${mapped_pairs}
-pcr_dupe_pairs=`samtools view -q $qualthresh -u -f 0x40 -F 2316 ${outprefix}-PT.sorted.bam | samtools view -f 0x400 | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
+mapped_pairs=`samtools view -q $qualthresh -f 0x40 -F 2316 ${outprefix}-PT.bam | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
+pcr_dupe_pairs=`samtools view -q $qualthresh -u -f 0x40 -F 2316 ${outprefix}-PT.bam | samtools view -f 0x400 | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
 
-mapped_nondupe_pairs=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.sorted.bam | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
-mapped_nondupe_pairs_cis=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.sorted.bam | awk '{if (sqrt($9^2) > 0) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
-mapped_nondupe_pairs_cis_lt1000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.sorted.bam | awk '{if ((sqrt($9^2) <= 1000) && ($9 != 0)) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
-mapped_nondupe_pairs_cis_gt1000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.sorted.bam | awk '{if (sqrt($9^2) > 1000) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
-mapped_nondupe_pairs_cis_gt10000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.sorted.bam | awk '{if (sqrt($9^2) > 10000) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
-mapped_trans_pairs=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.sorted.bam | awk '{if (sqrt($9^2) == 0) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
+mapped_nondupe_pairs=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
+mapped_nondupe_pairs_cis=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 0) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
+mapped_nondupe_pairs_cis_lt1000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if ((sqrt($9^2) <= 1000) && ($9 != 0)) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
+mapped_nondupe_pairs_cis_gt1000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 1000) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
+mapped_nondupe_pairs_cis_gt10000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 10000) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
+mapped_trans_pairs=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) == 0) { print; }}' | perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' | wc -l`
 
 valid_pairs=$(($mapped_nondupe_pairs_cis_gt1000 + $mapped_trans_pairs))
 
