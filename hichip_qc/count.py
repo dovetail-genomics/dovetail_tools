22363011a1502a11863284cea504ceae3d209797
Ubuntu <ubuntu@ip-172-31-53-84.us-west-2.compute.internal>
2020-06-30 13:16:04 +0000

    count expected/observed

--- hichip_qc/count.py
+++ hichip_qc/count.py
@@ -19,13 +19,11 @@ def get_read_count(bedfile):
     return count
 
 
-def get_snr(in_peaks, paired_reads, bp_in_loops , ref_length, number_of_loops, padding):
-    loop_in_length = 2*padding*number_of_loops + bp_in_loops
-    outside_loop_length = ref_length - loop_in_length
-    coverage_in_loop = 2*150*in_peaks/loop_in_length
-    coverage_outside_loop = 2*150*(paired_reads - in_peaks)/outside_loop_length
-    snr_ratio = round(coverage_in_loop/coverage_outside_loop,2)
-    return snr_ratio
+def get_expected_observed(num_peaks, window_size, ref_length, reads_total, reads_observed):
+    prob_read = num_peaks*window_size/ref_length
+    expected_reads = prob_read*reads_total
+    observed_expected_ratio = reads_observed/expected_reads
+    return observed_expected_ratio
 
 
 if __name__ == "__main__":
@@ -34,7 +32,6 @@ if __name__ == "__main__":
     parser.add_argument('-b2',help="bedfile2")
     parser.add_argument('-b3',help="bedfile3")
     parser.add_argument('-b4',help="bedfile4")
-    parser.add_argument('-b5',help="bedfile5")
     parser.add_argument('-peaks',help="input peaks")
     parser.add_argument('-bam',help="bamfile")
     args = parser.parse_args()
@@ -44,13 +41,11 @@ if __name__ == "__main__":
     in_500_peaks = get_read_count(args.b2)
     in_1000_peaks = get_read_count(args.b3)
     in_2000_peaks = get_read_count(args.b4)
-    in_5000_peaks = get_read_count(args.b5)
 
     in_peaks_fmt = format(in_peaks,",d")
     in_500_peaks_fmt = format(in_500_peaks,",d")
     in_1000_peaks_fmt = format(in_1000_peaks,",d")
     in_2000_peaks_fmt = format(in_2000_peaks,",d")
-    in_5000_peaks_fmt = format(in_5000_peaks, ",d")
 
     bamfile = pysam.AlignmentFile(args.bam,'rb')
 
@@ -76,12 +71,17 @@ if __name__ == "__main__":
          if r.is_paired:
             paired_reads += 1
 
+    
+    ratio_in_peaks = round(get_expected_observed(number_of_loops,np.mean(peak_size), ref_length, paired_reads, in_peaks), 2)
+    ratio_in_500_peaks = round(get_expected_observed(number_of_loops,np.mean(peak_size) + 500, ref_length, paired_reads, in_500_peaks),2)
+    ratio_in_1000_peaks = round(get_expected_observed(number_of_loops,np.mean(peak_size)+ 1000, ref_length, paired_reads, in_1000_peaks),2)
+    ratio_in_2000_peaks = round(get_expected_observed(number_of_loops,np.mean(peak_size)+ 2000, ref_length, paired_reads, in_2000peaks),2)
+    
     total_valid_pairs = paired_reads//2
     in_peaks_p = round(in_peaks *100.0/total_valid_pairs,2)
     in_500_peaks_p = round(in_500_peaks *100.0/total_valid_pairs,2)
     in_1000_peaks_p = round(in_1000_peaks *100.0/total_valid_pairs,2)
     in_2000_peaks_p = round(in_2000_peaks *100.0/total_valid_pairs,2)
-    in_5000_peaks_p = round(in_5000_peaks *100.0/total_valid_pairs,2)
     median_peak_size = format(int(np.median(peak_size)),",d")
     mean_peak_size = format(int(np.mean(peak_size)), ",d")
     number_of_loops = format(number_of_loops, ",d")
@@ -93,4 +93,7 @@ if __name__ == "__main__":
     print(f"Total read pairs in 500 bp around peaks:\t{in_500_peaks_fmt}({in_500_peaks_p}%)")
     print(f"Total read pairs in 1000 bp around peaks:\t{in_1000_peaks_fmt}({in_1000_peaks_p}%)")
     print(f"Total read pairs in 2000 bp around peaks:\t{in_2000_peaks_fmt}({in_2000_peaks_p}%)")
-    print(f"Total read pairs in 5000 bp around peaks:\t{in_5000_peaks_fmt}({in_5000_peaks_p}%)")
+    print(f"Observed/Expected ratio for reads in peaks:\t{ratio_in_peaks}")
+    print(f"Observed/Expected ratio for reads in 500bp around  peaks:\t{ratio_in_500_peaks}")
+    print(f"Observed/Expected ratio for reads in 1000bp around  peaks:\t{ratio_in_1000_peaks}")
+    print(f"Observed/Expected ratio for reads in 2000bp around  peaks:\t{ratio_in_2000_peaks}")
