a9e2cfd3e676f0014ae34aed18f73d2e0a657399
Ubuntu <ubuntu@ip-172-31-1-200.us-west-2.compute.internal>
2020-07-15 19:48:11 +0000

    phasing scripts added

---
+++ snps/phasing_workflow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [  $# -le 2 ]
+then
+        echo "Too few arguments. Please provide all the required arguments."
+        echo "Usage: ./phasing_workflow.sh <variants_vcf> <alignment_bam> <output_prefix>"
+        exit 1
+fi
+
+vcf=$1
+bam=$2
+prefix=$3
+
+#Phase these variants with HAPCUT2
+
+grep -E '^#|0/0|1/1|0/1|1/0|0/2|2/0'  ${vcf} > ${prefix}_filtered_variants.vcf
+
+extractHAIRS  --hic 1 --bam ${bam} --VCF ${prefix}_filtered_variants.vcf --out ${prefix}_hapcut.fragments
+HAPCUT2 --hic 1 --fragments ${prefix}_hapcut.fragments --VCF ${prefix}_filtered_variants.vcf --output ${prefix}_hapcut_output --outvcf 1 
+
+bgzip -c ${prefix}_hapcut_output.phased.VCF  > ${prefix}_hapcut_output.phased.VCF.gz
+tabix ${prefix}_hapcut_output.phased.VCF.gz
+
