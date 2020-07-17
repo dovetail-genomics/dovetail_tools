93ba39503e505f84ade9f1eb5e1fbeca025a4f13
Ubuntu <ubuntu@ip-172-31-1-200.us-west-2.compute.internal>
2020-07-17 19:21:58 +0000

    hq region updated

--- snps_and_phasing/make_hq_region.sh
+++ snps_and_phasing/make_hq_region.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [  $# -le 5 ]
+if [  $# -le 4 ]
 then
         echo "Too few arguments. Please provide all the required arguments."
         echo "Usage: ./make_hq_region.sh <aligment_bam> <snps_vcf.gz> <GIAB_confident_regions_bed> <output_prefix> <output_directory>"
@@ -17,7 +17,7 @@ output_dir=$5
 
 
 ${DOVETOOLS}/get_HQ_region_bed.py -bam ${bam}  -bedroot ${output_dir}/${output_prefix}
-bedtools intersect -a ${hq_region} -b ${output_dir}/${output_prefix}_highconf.bed > ${output_dir}/ConfidentRegionsAND${output_prefix}HQ.bed
+bedtools intersect -a ${hq_region} -b ${output_dir}/${output_prefix}_highconf.bed | grep -v '*' >  ${output_dir}/ConfidentRegionsAND${output_prefix}HQ.bed
 bedtools intersect -header -a ${snps} -b ${output_dir}/ConfidentRegionsAND${output_prefix}HQ.bed > ${output_dir}/${output_prefix}_highconf.vcf
 bgzip ${output_dir}/${output_prefix}_highconf.vcf
 tabix -p vcf ${output_dir}/${output_prefix}_highconf.vcf.gz
