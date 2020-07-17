fa717216e4b669ee15f57be3bf05988ef53c98e6
Ubuntu <ubuntu@ip-172-31-1-200.us-west-2.compute.internal>
2020-07-17 18:58:29 +0000

    removing files

--- snps/phasing_eval.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hapcut_phasing=$1
-truth_phasing=$2
-out_prefix=$3
-
-whatshap compare --only-snvs --switch-error-bed ${out_prefix}_switch_erros.txt --tsv-pairwise ${out_prefix}_phasing_eval.txt ${truth_phasing} ${hapcut_phasing} 
