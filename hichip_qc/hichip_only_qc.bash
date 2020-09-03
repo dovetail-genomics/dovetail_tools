98d8030e1b12c786c66a9f0f33a574a92e35d8d2
Ubuntu <ubuntu@ip-172-31-1-200.us-west-2.compute.internal>
2020-09-03 17:19:52 +0000

    hichip only qc added

---
+++ hichip_qc/hichip_only_qc.bash
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+
+if [  $# -le 5 ]
+then
+        echo "Too few arguments. Please provide all the required arguments."
+        echo "Usage: ./hichip_qc.bash <reference_fasta> <bam_file>  <chipseq_peaks>  <output_prefix> <num_cores>"
+        exit 1
+fi
+
+ref=$1
+bam=$2
+peaks=$3
+prefix=$4
+cores=$5
+
+sample=`basename ${prefix}`
+SRCDIR=`dirname $0`
+
+OUTPUTFILE=${prefix}"_hichip_qc_metrics.txt"
+TMPOUT=${prefix}"_hichip_qc_metrics.txt.tmp"
+#first run omnic_qc
+
+
+genome=${prefix}.genome
+#${SRCDIR}/../omni-c_qc.bash ${ref} ${r1fq} ${r2fq} ${prefix}  ${sample} ${cores} > ${TMPOUT}
+
+
+
+bedtools sort -g ${genome} -i ${peaks} > ${prefix}_reordered_peaks.bed
+
+bed=${prefix}"_reordered_peaks.bed"
+
+bed_chr20=${prefix}"_chr20_reordered_peaks.bed"
+grep -w 'chr20' ${bed} > ${bed_chr20}
+
+#find reads in the blacklisted region
+
+bedtools intersect -a ${bam} -b ${SRCDIR}/hg38.blacklist.bed -bed | sort -k4 > ${prefix}_blacklist_intersect.bed 
+
+#compute how many reads intersect with peaks
+bedtools intersect -a ${bam} -b ${bed} -bed | sort -k4 > ${prefix}_peak_intersect.bed &
+bedtools window -w 250 -abam ${bam} -b ${peaks} -bed | sort -k4 > ${prefix}_peaks_intersect_500.bed &
+bedtools window -w 500 -abam ${bam} -b ${peaks} -bed | sort -k4 > ${prefix}_peaks_intersect_1000.bed &
+bedtools window -w 1000 -abam ${bam} -b ${peaks} -bed | sort -k4 > ${prefix}_peaks_intersect_2000.bed &
+
+wait
+
+#generate peak enrichment plot
+
+bamCoverage --bam ${bam} --outFileName  ${prefix}_coverage.bigwig --outFileFormat bigwig -p ${cores}& 
+
+#plotFingerprint -b ${bam} --region chr20 --plotFile ${prefix}_chip_fingerprint_plot.png --outRawCounts ${prefix}_counts.tab &
+
+
+wait 
+
+#python ${SRCDIR}/plot_chip_fingerprint.py -table ${prefix}_counts.tab -output ${prefix}_chip_fingerprint_plot.png 
+python ${SRCDIR}/plot_chip_enrichment.py -bam ${bam} -peaks ${peaks} -output ${prefix}_chip_enrichment_plot.png
+
+
+#print final stats
+python ${SRCDIR}/count.py -b1  ${prefix}_peak_intersect.bed -b2 ${prefix}_peaks_intersect_500.bed \
+	-b3 ${prefix}_peaks_intersect_1000.bed -b4 ${prefix}_peaks_intersect_2000.bed \
+	-b5 ${prefix}_blacklist_intersect.bed -bam ${bam} -peaks ${peaks} >> ${TMPOUT}
+
+python  ${SRCDIR}/make_report.py -i ${TMPOUT} -r  ${prefix} -o ${OUTPUTFILE}
+
+rm ${prefix}_reordered_peaks.bed  ${bed_chr20}  ${prefix}_peak_intersect.bed ${prefix}_peaks_intersect_500.bed \
+	${prefix}_peaks_intersect_1000.bed ${prefix}_peaks_intersect_2000.bed ${TMPOUT}
