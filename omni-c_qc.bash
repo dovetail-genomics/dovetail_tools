5a519e7b7fefb0a823c52593ee442c312bddfb3d
Ubuntu <ubuntu@ip-172-31-1-200.us-west-2.compute.internal>
2020-10-13 13:17:10 +0000

    added latest pairtools parameters

--- omni-c_qc.bash
+++ omni-c_qc.bash
@@ -30,11 +30,21 @@ bwa mem -5SP -T0 -t${cores} \
     $ref \
     $fq1 \
     $fq2 \
-| pairtools parse --chroms-path ${genome}\
-| pairtools sort  --nproc ${cores} \
-| pairtools dedup --nproc-in ${cores} --nproc-out ${cores} --mark-dups  \
- 	--output-stats ${outprefix}-PT.stats.txt --output-dups - \
-| pairtools split --nproc-in ${cores} --nproc-out ${cores} \
+| pairtools parse \
+	--chroms-path ${genome} \
+	--min-mapq 40 \
+	--walks-policy 5unique \
+	--max-inter-align-gap 30 \
+      	--nproc-in ${cores} --nproc-out ${cores} \
+| pairtools sort  \
+	--nproc ${cores} \
+| pairtools dedup \
+	--nproc-in ${cores} --nproc-out ${cores} \
+	--mark-dups  \
+ 	--output-stats ${outprefix}-PT.stats.txt \
+	--output-dups - \
+| pairtools split \
+	--nproc-in ${cores} --nproc-out ${cores} \
 	--output-pairs ${outprefix}.PT.pairs.gz  \
 	--output-sam - \
 | ${SRCDIR}/add_mate_MQ.py \
@@ -45,39 +55,39 @@ bwa mem -5SP -T0 -t${cores} \
 samtools index ${outprefix}-PT.bam;
 
 
-preseq lc_extrap -B -P -e 2.1e9 -s 1e8 -seg_len 1000000000 -o $outprefix.preseq ${outprefix}-PT.bam
+#preseq lc_extrap -B -P -e 2.1e9 -s 1e8 -seg_len 1000000000 -o $outprefix.preseq ${outprefix}-PT.bam
 
-ps300m=`cat ${outprefix}.preseq | grep -P "^300000000.0" | awk '{print $2}'`
+#ps300m=`cat ${outprefix}.preseq | grep -P "^300000000.0" | awk '{print $2}'`
 
-qualthresh=40
-mate_filter_cmd="perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' $qualthresh"
+#qualthresh=40
+#mate_filter_cmd="perl -e 'while (<STDIN>) { m/MQ:i:(\\d+)/; if (\$1 >= \$ARGV[0]) { print; }}' $qualthresh"
 
-r1=`samtools view -c -q $qualthresh -f 0x40 -F 2304 ${outprefix}-PT.bam`
-r2=`samtools view -c -q $qualthresh -f 0x80 -F 2304 ${outprefix}-PT.bam`
+#r1=`samtools view -c -q $qualthresh -f 0x40 -F 2304 ${outprefix}-PT.bam`
+#r2=`samtools view -c -q $qualthresh -f 0x80 -F 2304 ${outprefix}-PT.bam`
 
-mapped_pairs=`samtools view -q $qualthresh -f 0x40 -F 2316 ${outprefix}-PT.bam | eval $mate_filter_cmd | wc -l`
-pcr_dupe_pairs=`samtools view -q $qualthresh -u -f 0x40 -F 2316 ${outprefix}-PT.bam | samtools view -f 0x400 | eval $mate_filter_cmd | wc -l`
+#mapped_pairs=`samtools view -q $qualthresh -f 0x40 -F 2316 ${outprefix}-PT.bam | eval $mate_filter_cmd | wc -l`
+#pcr_dupe_pairs=`samtools view -q $qualthresh -u -f 0x40 -F 2316 ${outprefix}-PT.bam | samtools view -f 0x400 | eval $mate_filter_cmd | wc -l`
 
-mapped_nondupe_pairs=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | eval $mate_filter_cmd | wc -l`
-mapped_nondupe_pairs_cis=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 0) { print; }}' | eval $mate_filter_cmd | wc -l`
-mapped_nondupe_pairs_cis_lt1000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if ((sqrt($9^2) <= 1000) && ($9 != 0)) { print; }}' | eval $mate_filter_cmd | wc -l`
-mapped_nondupe_pairs_cis_gt1000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 1000) { print; }}' | eval $mate_filter_cmd | wc -l`
-mapped_nondupe_pairs_cis_gt10000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 10000) { print; }}' | eval $mate_filter_cmd | wc -l`
-mapped_trans_pairs=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) == 0) { print; }}' | eval $mate_filter_cmd | wc -l`
+#mapped_nondupe_pairs=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | eval $mate_filter_cmd | wc -l`
+#mapped_nondupe_pairs_cis=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 0) { print; }}' | eval $mate_filter_cmd | wc -l`
+#mapped_nondupe_pairs_cis_lt1000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if ((sqrt($9^2) <= 1000) && ($9 != 0)) { print; }}' | eval $mate_filter_cmd | wc -l`
+#mapped_nondupe_pairs_cis_gt1000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 1000) { print; }}' | eval $mate_filter_cmd | wc -l`
+#mapped_nondupe_pairs_cis_gt10000=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) > 10000) { print; }}' | eval $mate_filter_cmd | wc -l`
+#mapped_trans_pairs=`samtools view -q $qualthresh -f 0x40 -F 3340 ${outprefix}-PT.bam | awk '{if (sqrt($9^2) == 0) { print; }}' | eval $mate_filter_cmd | wc -l`
 
-valid_pairs=$(($mapped_nondupe_pairs_cis_gt1000 + $mapped_trans_pairs))
+#valid_pairs=$(($mapped_nondupe_pairs_cis_gt1000 + $mapped_trans_pairs))
 
 
-echo "Mapping Quality Threshold         :" $qualthresh
-echo "Read1                             :" `echo $r1 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Read2                             :" `echo $r2 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Mapped pairs                      :" `echo $mapped_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "PCR dupe pairs                    :" `echo $pcr_dupe_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Mapped nondupe pairs              :" `echo $mapped_nondupe_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Valid Pairs (cis>1000bp + trans)  :" `echo $valid_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Mapped nondupe pairs cis          :" `echo $mapped_nondupe_pairs_cis | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Mapped nondupe pairs cis <=1000bp :" `echo $mapped_nondupe_pairs_cis_lt1000 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Mapped nondupe pairs cis >1000bp  :" `echo $mapped_nondupe_pairs_cis_gt1000 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Mapped nondupe pairs cis >10000bp :" `echo $mapped_nondupe_pairs_cis_gt10000 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Mapped nondupe trans pairs        :" `echo $mapped_trans_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
-echo "Expected unique pairs at 300M sequencing: " $ps300m
+#echo "Mapping Quality Threshold         :" $qualthresh
+#echo "Read1                             :" `echo $r1 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Read2                             :" `echo $r2 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Mapped pairs                      :" `echo $mapped_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "PCR dupe pairs                    :" `echo $pcr_dupe_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Mapped nondupe pairs              :" `echo $mapped_nondupe_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Valid Pairs (cis>1000bp + trans)  :" `echo $valid_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Mapped nondupe pairs cis          :" `echo $mapped_nondupe_pairs_cis | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Mapped nondupe pairs cis <=1000bp :" `echo $mapped_nondupe_pairs_cis_lt1000 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Mapped nondupe pairs cis >1000bp  :" `echo $mapped_nondupe_pairs_cis_gt1000 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Mapped nondupe pairs cis >10000bp :" `echo $mapped_nondupe_pairs_cis_gt10000 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Mapped nondupe trans pairs        :" `echo $mapped_trans_pairs | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'`
+#echo "Expected unique pairs at 300M sequencing: " $ps300m
